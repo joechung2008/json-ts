c0acd847d5d54ecd0f7a8aebebdefcd702164bb9
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-11 16:01:51 -0400

    Export app for Vercel
    - Do not auto-start api-express server on Vercel

--- packages/api-express/src/index.ts
+++ packages/api-express/src/index.ts
@@ -16,7 +16,11 @@ app.post("/api/v1/parse", (req, res) => {
   }
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+if (process.env.VERCEL !== undefined) {
+  const port = 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
+export default app;
