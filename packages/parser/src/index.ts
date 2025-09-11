c1d8b533938bfcebc521de23a1785a01f6a195e5
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-11 15:30:00 -0400

    Use tsup to build parser

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,1 +1,15 @@
 export { parse } from "./json.js";
+
+export type {
+  ArrayToken,
+  FalseToken,
+  NullToken,
+  NumberToken,
+  ObjectToken,
+  PairToken,
+  StringToken,
+  Token,
+  TrueToken,
+  Type,
+  ValueToken,
+} from "./json.js";
---
+++ packages/parser/tsup.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tsup').Options} */
+export default {
+  dts: true,
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  outDir: "dist",
+  sourcemap: true,
+  target: "esnext",
+};
