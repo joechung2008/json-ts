60f760dd2f400be1e3a4be14cd6929ce11a62f04
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-16 19:38:06 -0400

    Use morgan middleware in api-express

--- packages/api-express/src/index.ts
+++ packages/api-express/src/index.ts
@@ -1,7 +1,9 @@
 import { parse } from "@json-ts/parser";
 import express, { type Express } from "express";
+import morgan from "morgan";
 
 const app: Express = express();
+app.use(morgan("combined"));
 app.use(express.text());
 
 app.post("/api/v1/parse", (req, res) => {
